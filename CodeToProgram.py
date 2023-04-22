8dc0d5b6d2136c02fbcae9bba655c7e44d99d98f
Alessandro Margonari <alessandromargonari@gmail.com>
2023-04-22 14:25:16 +0200

    Project import

---
+++ CodeToProgram.py
@@ -0,0 +1,8 @@
+import PyInstaller.__main__
+
+# Pass the script file as the first argument
+PyInstaller.__main__.run([
+    'E:\Desktop\Projects\py\systemInfoDesktop.py',
+    '--onefile',
+    '--name=System Info'
+])
